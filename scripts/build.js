6ca3ea341a1dcb60477600bd884f11abb46677a4
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-09-23 10:16:46 +0300

    Fix build script

--- scripts/build.js
+++ scripts/build.js
@@ -10,7 +10,7 @@ fs.readFile('node_modules/stylefmt/lib/params.js', 'utf-8', (err, data) => {
   console.log('File is read.');
 
   data = data
-    .replace('// same as the options in stylelint', 'if (options.skip) { return Promise.resolve(options) }')
+    .replace('function params (options) {', 'function params (options) {\n  if (options.skip) { return Promise.resolve(options) }')
     .replace('var cwd = process.cwd()', 'var cwd = options.cwd || process.cwd()');
 
   fs.writeFile('node_modules/stylefmt/lib/params.js', data, (err) => {
